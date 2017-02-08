e7ce93bbbb71e5c08c27eabe6611c49fcb65d955
EmineBasheva <eminka23@abv.bg>
2017-02-08 16:25:26 +0200

    add some help functions

--- benzaiten/libs/summarize.py
+++ benzaiten/libs/summarize.py
@@ -1,10 +1,13 @@
 from math import log10
 
+def get_sentences(text):
+    sentences = text.split(".")
+    return sentences
+
 def get_similarity(sentence1, sentence2):
-    sentence1 = sentence1.replace(".", "")
-    words_s1 = sentence1.split(" ")
-    sentence2 = sentence2.replace(".", "")
-    words_s2 = sentence2.split(" ")
+    ''' Get how similar are two sentences. '''
+    words_s1 = get_words(sentence1)
+    words_s2 = get_words(sentence2)
 
     common_words = set(set(words_s1) & set(words_s2))
     log_words1 = log10(len(words_s1))
@@ -13,7 +16,23 @@ def get_similarity(sentence1, sentence2):
     similarity = len(list(common_words)) / (log_words1 + log_words2)
     return similarity
 
+def get_words(sentence):
+    ''' Clear special symbols in the sentence and return words in list.'''
+
+    sentence = sentence.replace(".", "").replace("!", "").replace("?", "")
+    sentence = sentence.replace(",", "").replace(";", "").replace(":", "")
+    sentence = sentence.replace("\'", "").replace("\"", "").replace("-", "").replace("_", "").replace("+", "")
+    sentence = sentence.replace("(", "").replace(")", "").replace("[", "").replace("]", "").replace("{", "").replace("}", "")
+    sentence = sentence.replace("  ", " ")
+
+    words = sentence.split(" ")
+    return words
 
 if __name__ == "__main__":
+    sentences = get_sentences("Where is Maria? She go to the shop.")
+
+    words = get_words("Maria ? go to :the shop.")
+    print(words)
+
     n = get_similarity("Maria go to the shop.", "Georgi go to the gym.")
     print(n)
