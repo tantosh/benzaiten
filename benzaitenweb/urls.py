cf07ef6b62194cd7577b160e076728875b56fbf0
tantosh <tanya.h.tosheva@gmail.com>
2017-02-11 01:41:41 +0200

    add static to urls

--- benzaitenweb/urls.py
+++ benzaitenweb/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from benzaitenweb import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
