6b47fc1540606369bc81194de4211e35d7e4867f
tantosh <tanya.h.tosheva@gmail.com>
2017-02-10 14:01:10 +0200

    Use combinations instead of permutations

--- benzaiten/graph.py
+++ benzaiten/graph.py
@@ -1,6 +1,6 @@
 import re
 import random
-from itertools import permutations
+from itertools import combinations
 from heapq import nlargest
 from math import log10
 
@@ -24,7 +24,7 @@ class GraphBuilder:
         Similarity is computed for every pair of sentences. If the sentences are similar, the verteces are connected with weight equal to the similarity between them.
         '''
         verteces = [GraphNode(sentence, order) for order, sentence in enumerate(sentences)]
-        for vertex, other in permutations(verteces, 2):
+        for vertex, other in combinations(verteces, 2):
             similarity = self._similarity(vertex.sentence, other.sentence)
             if similarity:
                 vertex.connect(other, similarity)
@@ -34,16 +34,17 @@ class GraphBuilder:
         '''
         Computes how similar are two sentences. The number of common words and the length of the sentences is taken into consideration.
         '''
-        if sentence1 == '' or sentence2 == '':
+        if not sentence1 or not sentence2:
             return 0
         words_s1 = self._extract_words(sentence1)
         words_s2 = self._extract_words(sentence2)
 
         common_words = [w for w in words_s1 if w in words_s2]
-        log_words1 = log10(len(words_s1))
-        log_words2 = log10(len(words_s2))
-    
-        return round(len(common_words) / (log_words1 + log_words2), self._precision)
+        log_denominator = log10(len(words_s1)) + log10(len(words_s2))
+        
+        if not log_denominator:
+            return 0
+        return round(len(common_words) / log_denominator, self._precision)
     
     def _extract_sentences(self, text):
         ''' Extract sentences in a text '''
