298192f8b0fbef0654ac21637390b8e805ad073b
tantosh <tanya.h.tosheva@gmail.com>
2017-02-10 20:47:03 +0200

    Minor fix in view

--- benzaitenweb/views.py
+++ benzaitenweb/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from forms import SummaryForm
+from benzaitenweb.forms import SummaryForm
 
 def index(request):
     if request.method == 'POST':
         form = SummaryForm(request.POST)
         if form.is_valid():
             summary = 'this is a sample summary'
-            return render(request, 'index.html', {'summary': 
-                                                 summary = 'this is a sample summary'})
+            return render(request, 'index.html', {'summary': summary })
     else:
         form = SummaryForm()
     return render(request, 'index.html', {'form': form })
